120ffca2f6b4474e8b9b34f14b66dd46fbbf2e76
LariosDACTIC <Cristopher.Chavez@dactic.uni.edu.ni>
2024-06-06 13:20:56 -0600

    cambiando mensaje

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Esta es una prueba del Pipeline para MISSI 2024 en el curso de Operaciones de Ingenieria de Software");
     }
 }
