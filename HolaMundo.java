d6c2a839ff7d56143c8ef59f7644c77d63ac3d9a
LariosDACTIC <Cristopher.Chavez@dactic.uni.edu.ni>
2024-06-08 09:00:13 -0600

    Presentacion Final Practica MISSI

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,13 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
         System.out.println("Esta es una prueba del Pipeline para MISSI 2024 en el curso de Operaciones de Ingenieria de Software");
+
+        System.out.println("Integrantes: ");
+        System.out.println("Ing.Cristopher Larios");
+        System.out.println("Ing.Carlos Herrera");
+        System.out.println("Ing.Eugenio Diaz");
+        System.out.println("Ing.Abel Marin");
+
+        System.out.println("Gracias Por SU ATENCION");
     }
 }
